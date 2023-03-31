1ea4dd3db6c03291cb2e76d6e5d28098180b2121
Wilkie Olin-Ammentorp <wolinammentorp@gpulogin2.lcrc.anl.gov>
2023-03-31 16:18:40 -0500

    updating dispatch script

--- dispatch_script.jl
+++ dispatch_script.jl
@@ -9,7 +9,7 @@ run(cmd)
 params_file = "params_" * string(n_layers) * "_layers.p"
 #test it over the range of mask angles
 for angle in mask_angles
-    cmd = `srun --gres=gpu:1 -t 00:20:00 python test_script.py --n_layers $n_layers --mask_angle $angle --params_file $params_file &`
+    cmd = `sbatch --gres=gpu:1 -t 00:20:00 python test_script.py --n_layers $n_layers --mask_angle $angle --params_file $params_file`
     run(cmd)
     sleep(0.1)
 end
